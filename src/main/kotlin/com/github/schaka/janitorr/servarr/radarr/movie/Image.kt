6bd172d6ffa901e8ffdf91c8335ec65c6939e3cd
Schaka <2223171+Schaka@users.noreply.github.com>
2024-02-11 11:29:07 +0100

    Make url in Image nullable

--- src/main/kotlin/com/github/schaka/janitorr/servarr/radarr/movie/Image.kt
+++
@@ -1,7 +0,0 @@
-package com.github.schaka.janitorr.servarr.radarr.movie
-
-data class Image(
-    val coverType: String,
-    val remoteUrl: String,
-    val url: String
-)
--- src/main/kotlin/com/github/schaka/janitorr/servarr/sonarr/series/Image.kt
+++
@@ -1,7 +0,0 @@
-package com.github.schaka.janitorr.servarr.sonarr.series
-
-data class Image(
-    val coverType: String,
-    val remoteUrl: String,
-    val url: String
-)

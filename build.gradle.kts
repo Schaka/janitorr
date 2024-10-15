115d8372512521ac32dafc009ddd0b24104a0cce
Schaka <2223171+Schaka@users.noreply.github.com>
2024-10-15 10:28:24 +0200

    [Build] Try another way of passing encoding

--- build.gradle.kts
+++ build.gradle.kts
@@ -169,6 +169,7 @@ tasks.withType<BootBuildImage> {
         "BP_HEALTH_CHECKER_ENABLED" to "true",
         "BP_JVM_CDS_ENABLED" to "true",
         "BP_JVM_VERSION" to "23",
+        "BPE_NATIVE_IMAGE_OPTIONS" to "-Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8",
         "BPE_LANG" to "en_US.UTF-8",
         "BPE_LANGUAGE" to "LANGUAGE=en_US:en",
         "BPE_LC_ALL" to "en_US.UTF-8",
